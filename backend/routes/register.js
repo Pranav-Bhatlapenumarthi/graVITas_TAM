a4cde5e14d9a10a45e3d612958e2a509a2a99ee1
Shuvam Mitra <shuvammitra05@gmail.com>
2025-09-03 02:53:22 +0530

    db functions half done

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -1,24 +1,46 @@
 const express = require("express");
+const { supabase } = require("../lib/supabaseClient.js");
 const router = express.Router();
 
 function generateTeamID() {
-  return Math.floor(1000 + Math.random() * 9000).toString();
+	return Math.floor(1000 + Math.random() * 9000).toString();
 }
 
-router.post("/leader", (req, res) => {
-//   const { name, regNo, email, phone, teamName, numMembers } = req.body;
+// change this back to post
+router.get("/check", async (req, res) => {
+	// const { name, regNo, email, phone } = req.body;
+	const name = "Jackson West";
+	const regNo = "18LAPD33356";
+	const email = "iwantobecomepoliceman@lapd.com";
+	const phone = "7980455545";
+	
+	const { data, error } = await supabase
+		.from('users')
+		.insert({
+			name: name,
+			reg_no: regNo,
+			email: email,
+			phone: phone
+		})
 
-  res.json({ message: "Team created successfully" });
+	if (error) {
+		console.log(error);
+		return error;
+	} else {
+		console.log(data);
+	}
+	res.json({ message: "ghapla happened successfully" });
 });
 
 router.post("/member", (req, res) => {
-//   const { name, regNo, email, phone, teamID, teamName } = req.body;
+	//   const { name, regNo, email, phone, teamID, teamName } = req.body;
 
-  res.json({ message: "Member added successfully" });
+	res.json({ message: "Member added successfully" });
 });
 
 router.get("/teams", (req, res) => {
-  res.json(teams);
+	const teams = "holo bhai?";
+	res.json(teams);
 });
 
 module.exports = router;
