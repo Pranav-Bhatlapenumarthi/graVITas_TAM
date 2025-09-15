491ba3b3109924e2ffcf4f6845c136ea40e7da01
Shuvam Mitra <shuvammitra05@gmail.com>
2025-09-15 22:32:24 +0530

    handles multi-team edge case now

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -53,24 +53,63 @@ router.post("/team", async (req, res) => {
 	}
 });
 
+// router.post("/updateLeader", async (req, res) => {
+// 	const { teamcode, regNo } = req.body;
+	
+// 	const { data, error } = await supabase
+// 		.from("teams")
+// 		.update({ leader_reg_no: regNo })
+// 		.eq("team_id_code", teamcode)
+// 		.select("*")
+// 		;
+
+// 	if (error) {
+// 		console.log(error);
+// 		return error;
+// 	} else {
+// 		res.json({ 
+// 			teamcode: teamcode,
+// 			leader: regNo
+// 		});
+// 		console.log('ghapla');
+// 		console.log(data);
+// 	}
+// });
+
 router.post("/updateLeader", async (req, res) => {
 	const { teamcode, regNo } = req.body;
-	
+
 	const { data, error } = await supabase
-		.from("teams")
-		.update({ leader_reg_no: regNo })
-		.eq("team_id_code", teamcode)
-		.select("*")
+		.from('users')
+		.select('*')
+		.eq('teamcode', teamcode)
+		.eq('reg_no', regNo)
 		;
 
-	if (error) {
+	if (error || data.length==0) {
 		console.log(error);
 		return error;
 	} else {
-		res.json({ 
-			teamcode: teamcode,
-			leader: regNo
-		});
+		console.log(data[0]);
+		const { fdata, ferror } = await supabase
+			.from("teams")
+			.update({ leader_reg_no: data[0].user_id })
+			.eq("team_id_code", teamcode)
+			.select("*")
+			;
+
+		if (ferror) {
+			console.log(ferror);
+			return ferror;
+		} else {
+			res.json({ 
+				teamcode: teamcode,
+				leader: regNo
+			});
+			console.log('ghapla');
+			console.log(fdata);
+		}
+
 		console.log('ghapla');
 		console.log(data);
 	}
